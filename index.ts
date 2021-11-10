68356b9e95172f0f149cca91393dea2ff2eeb6f4
Sam-H <sam@iamasamwich.com>
2021-11-10 20:31:18 +1000

    redone from scratch

---
+++ index.ts
@@ -0,0 +1,127 @@
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
+import bcrypt from 'bcrypt';
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('listening on 3000');
+});
+
+const server = http.createServer(app);
+
+server.listen(3001, () => {
+  console.log('ws listening on 3001');
+});
+
+interface WsPlus extends WebSocket {
+  name ? : string | null;
+  room ? : string | null;
+  id ? : string | null;
+};
+
+interface Send {
+  type: string;
+  message: string;
+}
+
+interface TellRoom {
+  room : string | undefined;
+  type : string;
+  payload : string;
+};
+
+interface TellUser {
+  type: string;
+  payload: string | string[] | null;
+}
+
+const wss = new WebSocket.Server({server});
+
+wss.on('connection', async (ws : WsPlus) => {
+  const now = new Date().getTime().toString();
+  ws.id = await bcrypt.hash(now, 2);
+
+  const tellUser = ({type, payload} : TellUser) => {
+    ws.send(JSON.stringify({type, payload}));
+  };
+
+  const tellRoom = ({room, type, payload} : TellRoom) => {
+    wss.clients.forEach((client : WsPlus) => {
+      if (client.room === room && client !== ws) {
+        client.send(JSON.stringify({
+          type, payload
+        }));
+      };
+    });
+  };
+
+  const getRoomUsers = (room : string) => {
+    const users : string[] = [];
+    wss.clients.forEach((client : WsPlus) => {
+      if (client.room === room) {
+        users.push(client.name as string);
+      }
+    });
+    return users;
+  };
+
+  ws.on('message', (msg : Send) => {
+    const message = JSON.parse(msg.toString());
+
+    if (message.type === 'setName' && !ws.name) {
+      ws.name = message.message;
+      if (ws.room) {
+        tellRoom({
+          room: ws.room,
+          type: 'newMember',
+          payload: ws.name as string
+        });
+      } else {
+        tellUser({
+          type: 'setName',
+          payload: `${ws.name}`
+        });
+      };
+    };
+
+    if (message.type === "joinRoom" && ws.name) {
+      ws.room = message.message;
+      tellRoom({
+        room: ws.room as string,
+        type: 'newMember',
+        payload: ws.name as string
+      });
+      tellUser({
+        type: 'joinedRoom',
+        payload: `${ws.room}`
+      })
+      tellUser({
+        type: 'userList',
+        payload: getRoomUsers(ws.room as string)
+      })
+    };
+
+    if (message.type === 'leaveRoom' && ws.name && ws.room) {
+      tellRoom({
+        room: ws.room,
+        type: 'memberLeft',
+        payload: ws.name as string
+      });
+      tellUser({type: 'leftRoom', payload: null})
+      ws.room = null;
+    };
+
+    if (message.type === 'roomMessage' && ws.name && ws.room) {
+      tellRoom({
+        room: ws.room,
+        type: 'newMessage',
+        payload: `${ws.name}: ${message.message}`
+      });
+    };
+  });
+});
---
+++ public/main.js
@@ -0,0 +1,110 @@
+const nameField = document.querySelector('#name');
+const roomList = document.querySelector('#roomList');
+
+const ws = new WebSocket('ws://localhost:3001');
+
+ws.onopen = () => {
+  console.log('ws connected!');
+};
+
+ws.onmessage = ({data}) => {
+  const message = JSON.parse(data);
+  console.log(message);
+  if (message.type === 'setName') {
+    userName = message.payload;
+    renderName();
+  };
+
+  if (message.type === 'joinedRoom') {
+    selectedRoom = message.payload;
+  };
+};
+
+let userName = null;
+let selectedRoom = null;
+
+const renderName = () => {
+  nameField.innerHTML = '';
+
+  if (!userName) {
+    const inputField = document.createElement('div');
+    inputField.classList.add('input-field', 'col', 's8');
+
+    const input = document.createElement('input');
+    input.classList.add('validate');
+    input.setAttribute('id', 'nameSelect');
+    input.setAttribute('type', 'text');
+    input.setAttribute('placeholder', 'name');
+    inputField.appendChild(input);
+
+    const label = document.createElement('label');
+    label.classList.add('black-text');
+    label.setAttribute('for', 'nameSelect');
+    label.appendChild(document.createTextNode('Name'));
+    inputField.appendChild(label);
+
+    nameField.appendChild(inputField);
+
+    const button = document.createElement('div');
+    button.classList.add('btn', 'green', 'col', 's4');
+    button.appendChild(document.createTextNode('Register'));
+
+    button.addEventListener('click', () => {
+      if (!input.value) {
+        return;
+      };
+      console.log('registering ' + input.value);
+      ws.send(JSON.stringify({type: "setName", message: input.value}));
+    })
+
+    nameField.appendChild(button);
+  } else {
+    const h3 = document.createElement('h3');
+    h3.appendChild(document.createTextNode(`Your name: ${userName}`));
+
+    nameField.appendChild(h3);
+  };
+};
+
+const renderRooms = () => {
+
+  const rooms = ['Home', 'Chat', 'Trees'];
+
+  rooms.forEach(room => {
+    const li = document.createElement('li');
+    li.classList.add('collection-item', 'room', 'grey');
+    li.appendChild(document.createTextNode(room));
+    li.setAttribute('id', room);
+    roomList.appendChild(li);
+
+    li.addEventListener('click', () => {
+      if (li.id === selectedRoom) {
+        console.log('same room, returning');
+        return;
+      };
+      if (selectedRoom) {
+        ws.send(JSON.stringify({type: 'leaveRoom'}));
+      };
+      selectRoom(room);
+      ws.send(JSON.stringify({type: 'joinRoom', message: room}));
+    });
+  });
+};
+
+const selectRoom = (room) => {
+  const rooms = document.querySelectorAll('.room');
+  for (const li of rooms) {
+    if (li.id === room) {
+      li.classList.remove('grey');
+      li.classList.add('green');
+    } else {
+      li.classList.add('grey');
+      li.classList.remove('green');
+    };
+  };
+};
+
+
+
+renderName();
+renderRooms();
